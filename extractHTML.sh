183fdb68f1b2f47eb23353622e0a35a4e7990c4c
Elliot <elliot.james.williams@gmail.com>
2018-03-22 18:12:11 -0400

    Finished implementation so it works for as many files as I care to give it, with cleaning up of directories for information purposes

--- extractHTML.sh
+++ extractHTML.sh
@@ -1,5 +1,11 @@
-for file in ./input_wikidump/*
+for file in ./input_wikidump/*.bz2
 do
-  python WikiExtractor.py -o ./html_extract --html --no-templates "$file"
-  rm "$file"
+  echo "Running script for $file"
+  echo "Running Wikipedia extractor... (will take 5-10 min)";
+  python WikiExtractor.py -o ./html_extract -q --html --no-templates "$file" > log/wiki.log 2>log/wiki_err.log;
+  rm "$file";
+  echo "Running Link Extractor...";
+  python extract_links.py "$file" > log/extractLinks.log 2> log/extractLinks_err.log;
+  rm -rf ./link_output/*.dat;
+  rm -rf ./html_extract/*/;
 done
--- extract_links.py
+++ extract_links.py
@@ -5,6 +5,31 @@ from lxml import html
 import re
 import matplotlib.pyplot as plt
 from urllib.parse import unquote
+import fcntl
+import threading
+import random
+import shutil
+import sys
+import glob
+
+
+def writeLinksToFile(article_results):
+    rand_bits = random.getrandbits(100)
+    f = open('./link_output/link_file_{}.dat'.format(rand_bits), 'w')
+    for article in article_results:
+        title = article[0]
+        f.write(">>>>{}\n".format(title))
+        links = article[1]
+        for link in links:
+            f.write(link+"\n")
+
+def cleanupLinkfiles(filename):
+    outfilename = "./link_files/{}_links.dat".format(filename)
+    with open(outfilename, 'wb') as outfile:
+        for filename in glob.glob('./link_output/*.dat'):
+            with open(filename, 'rb') as readfile:
+                shutil.copyfileobj(readfile, outfile)
+
 
 @ray.remote
 def parseWikidumpFile(file_path):
@@ -19,35 +44,40 @@ def parseWikidumpFile(file_path):
     contents = re.sub("%29", ")", contents)
     contents = re.sub("%2C", ",", contents)
 
+    # Each wiki_ file output by WikiExtractor can contain multiple articles
+    # This code splits each file into its constitutent articles
     splitter = "<doc"
     doc_list = [splitter + doc for doc in contents.split(splitter)]
     doc_list = list(filter(lambda x : x.strip() != "<doc", doc_list))
-    link_num = []
+
+    article_results = []
+
+    # For each document in the doc_list associated with each file,
+    # we scrape the article title and all links contained therein
     for doc in doc_list:
         # Opens HTML representation of file contents in lxml
         tree = html.fromstring(doc)
-        # Gets id numbers associated with each article in the file
-        titles = tree.xpath('//doc/@title')
+        # Gets title associated with the particular article
+        title = tree.xpath('//doc/@title')[0]
 
+        # Gets all links within the article
         links = tree.xpath("//a/@href")
-        links = list(filter(lambda x: not re.match("https?://", x), links))
+        # Filters out links not internal to Wikipedia
+        links = list(filter(lambda x: not re.match("https?:\/\/", x), links))
+        # Decodes special characters (ie from Poincar%C3%A9 to correct format)
         links = [unquote(link) for link in links]
-        link_num.append(len(links))
 
-    # Seems to have a power law distribution, we'll see
-    print("{} links for {} docs for average degree of {}".format(sum(link_num), len(link_num), sum(link_num)/len(link_num)))
-    # plt.hist(link_num)
-    # plt.show()
-    return (sum(link_num) / len(link_num))
+        article_results.append([title, links])
 
-@ray.remote
-def parseWikidumpDir(dir_path):
-    pass
+    writeLinksToFile(article_results)
+
 
 if __name__ == "__main__":
     ray.init()
 # Gets all subdirectories, and all files within those subdirectories,
 # using os.walk
+    filename = sys.argv[1]
+    filename = re.sub(".*\/", "", filename)
     html_dir_list = list(os.walk("./html_extract"))[1:]
     for subdir_tuple in html_dir_list:
         dir_path = subdir_tuple[0] # the path to the subdirectory for dir_tuple
@@ -56,4 +86,4 @@ if __name__ == "__main__":
         article_results = [dir_path + "/" + filename for filename in file_list]
         article_results = [parseWikidumpFile.remote(x) for x in article_results]
         results = ray.get(article_results)
-        print("Overall average degree is {}".format(sum(results) / len(results)))
+    cleanupLinkfiles(filename)
