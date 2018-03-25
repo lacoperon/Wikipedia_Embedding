23f227d14b7cc1a4c80071e2c9d97656f9c1b388
Elliot <elliot.james.williams@gmail.com>
2018-03-25 13:58:31 -0400

    Removed --no-templates option, I suspect that was throwing away information

--- extractHTML.sh
+++ extractHTML.sh
@@ -2,7 +2,7 @@ for file in ./input_wikidump/*.bz2
 do
   echo "Running script for $file"
   echo "Running Wikipedia extractor... (will take 5-10 min)";
-  python WikiExtractor.py -o ./html_extract -q --html --no-templates "$file" > log/wiki.log 2>log/wiki_err.log;
+  python WikiExtractor.py -o ./html_extract -q --html "$file" > log/wiki.log 2>log/wiki_err.log;
   rm "$file";
   echo "Running Link Extractor...";
   python extract_links.py "$file" > log/extractLinks.log 2> log/extractLinks_err.log;
